21a9f260da2a557297fc5c7320a9370ad036ec1b
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-09-05 20:39:38 -0300

    Creating style divs

--- src/components/UnitySelection/index.jsx
+++ src/components/UnitySelection/index.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { Container, Buttons, Button } from "./styled";
+export function UnitySelection({ handleSetToggleRenderToCalculator }) {
 
-export function UnitySelection({handleSetToggleRenderToCalculator}) {
 
-
-    return(
+    return (
         <>
-        <h3>Quantidade de unidades:</h3>
-        <button onClick={handleSetToggleRenderToCalculator}>2 Unidades</button>
-        <button onClick={handleSetToggleRenderToCalculator}>3 Unidades</button>
+            <Container>
+                <h3>Quantidade de unidades:</h3>
+                <Buttons>
+                    <Button>
+                        <p>um butaum</p>
+                        <button className="butaum" onClick={handleSetToggleRenderToCalculator}>2 Unidades</button>
+                    </Button>
+                    <Button>
+                        <p>oto butaum</p>
+                        <button className="butaum" onClick={handleSetToggleRenderToCalculator}>3 Unidades</button>
+                    </Button>
+                </Buttons>
+
+            </Container>
+
         </>
     )
 }
