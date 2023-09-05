60fddd1825d438acc536ccfe7cc5b32cffb93ce1
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-09-05 20:40:03 -0300

    Creating style for unity section

---
+++ src/components/UnitySelection/styled.js
@@ -0,0 +1,40 @@
+import { styled } from "styled-components"
+
+export const Container = styled.div`
+margin-top: 1rem;
+
+    
+`
+
+export const Buttons = styled.div`
+    padding:0.6rem;
+    max-height: 200px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    width: 100%;
+    
+    
+`
+export const Button = styled.div`
+margin-top: 1rem;
+background-color: #e1edf2;
+    border-radius: 0.25rem;
+    padding: 0.8rem;
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+    button {
+        color: white;
+
+        border: 0;
+        border-radius: 0.25rem;
+        background-color: white;
+        padding: 0.75rem;
+    }
+    .butaum {
+        background-color: #164194;
+    }
+
+`
