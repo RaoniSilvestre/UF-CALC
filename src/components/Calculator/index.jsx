5fb4f4fc189fa3d21ad550bef2cc94b3a104931f
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-09-05 20:38:03 -0300

    Mean initial logic

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,8 +1,26 @@
-export function Calculator({handleSetToggleRenderToUnitySelection}){
-    return(
+export function Calculator({ handleSetToggleRenderToUnitySelection }) {
+
+
+    function calcularMedia(N1, N2, N3) {
+        var media = 5;
+        var mediaDoAluno = (N1 + N2 + N3) / 3
+        if (N1 == null) {
+            alert("Digite um valor válido.");
+        } else {
+            if ((N1 < 3 || N2 < 3) || N3 < 3) {
+                media = 7;
+            }
+            if (mediaDoAluno >= media) {
+                alert("Você passou!");
+            } else {
+                alert("Você tem problemas!");
+            }
+        }
+    }
+    return (
         <>
-        <p>Aqui vai a calculadora</p>
-        <button onClick={handleSetToggleRenderToUnitySelection}>Voltar</button>
+            <p>Aqui vai a calculadora</p>
+            <button onClick={handleSetToggleRenderToUnitySelection}>Voltar</button>
         </>
     )
 }
