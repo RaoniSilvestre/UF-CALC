a6e4bda10f2338b358c69fc5840479c7f110ebe4
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-09-05 19:04:00 -0300

    Creating Calculator component
    And button for toggle between components

---
+++ src/components/Calculator/index.jsx
@@ -0,0 +1,8 @@
+export function Calculator({handleSetToggleRenderToUnitySelection}){
+    return(
+        <>
+        <p>Aqui vai a calculadora</p>
+        <button onClick={handleSetToggleRenderToUnitySelection}>Voltar</button>
+        </>
+    )
+}
