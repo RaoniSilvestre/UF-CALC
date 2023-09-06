81520ab151dc63a92c4743105c1ed488df7521a4
Raoni Silva <raoni.silvestre@hotmail.com>
2023-09-06 10:56:56 -0300

    Calculator component style fix

--- src/components/Calculator/styled.js
+++ src/components/Calculator/styled.js
@@ -1,1 +1,13 @@
 import styled from "styled-components"
+
+export const Retornar = styled.div`
+    display: flex;
+    align-items: start;
+    button {
+            color: white;
+            border: 0;
+            border-radius: 0.25rem;
+            background-color: #164194;
+            padding: 0.5rem;
+        }
+`
