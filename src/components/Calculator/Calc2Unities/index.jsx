f77fc87274c5a8f8c6d806d14a3c0fc93a58541b
Raoni Silva <raoni.silvestre@hotmail.com>
2023-09-06 10:55:03 -0300

    Mudando local da função calcular media2

--- src/components/Calculator/Calc2Unities/index.jsx
+++ src/components/Calculator/Calc2Unities/index.jsx
@@ -1,32 +1,9 @@
 import { useState } from "react";
-
+import { Container,Nota } from "./styled";
+import { calcularMedia2 } from "../../../features/CalcularNotas2";
 export function Calc2Unities(){
-    const [dado1, setDado1] = useState(1.0);
-    const [dado2, setDado2] = useState(1.0);
-
-
-    function calcularMedia2(N1, N2) {
-        var media = 5;
-
-        var mediaDoAluno = (parseFloat(N1) + parseFloat(N2))/2
-        if (N1 == null) {
-            alert("Digite um valor válido.");
-        } else {
-            if (N1 < 3 || N2 < 3) {
-                media = 7;
-            }
-            console.log(mediaDoAluno,media)
-            if (mediaDoAluno >= media) {
-                alert("Você passou!");
-            } else {
-                let maiorValor = Math.max(N1,N2)
-                let recNota = (media*2)-maiorValor
-                alert(`Você tem problemas! \nNota necessária na recuperação: ${recNota}`);
-            }
-        }
-    }
-
-
+    const [dado1, setDado1] = useState('');
+    const [dado2, setDado2] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -35,18 +12,25 @@ export function Calc2Unities(){
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    value={dado1}
-                    onChange={(e) => setDado1(e.target.value)} // 2. e 3.
-                    placeholder="Dado 1"
-                />
-                <input
-                    value={dado2}
-                    onChange={(e) => setDado2(e.target.value)} // 2. e 3.
-                    placeholder="Dado 2"
-                />
-                <button type="submit">Enviar</button>
-            </form>
+            <Container> 
+                <form onSubmit={handleSubmit}>
+                    <Nota>
+                        
+                    <input
+                        value={dado1}
+                        onChange={(e) => setDado1(e.target.value)} // 2. e 3.
+                        placeholder="Nota 1"
+                    />
+                    </Nota>
+                    <Nota>
+                    <input
+                        value={dado2}
+                        onChange={(e) => setDado2(e.target.value)} // 2. e 3.
+                        placeholder="Nota 2"
+                    />    
+                    </Nota>
+                    <button type="submit">Enviar</button>
+                </form>
+            </Container>
         </>)
 }
