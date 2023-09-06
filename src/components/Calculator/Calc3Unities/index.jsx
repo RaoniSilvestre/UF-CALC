b13b1631bc792555f7727e6ff80f98083f4adb0b
Raoni Silva <raoni.silvestre@hotmail.com>
2023-09-06 10:29:59 -0300

    changing calcularmedia3 place

--- src/components/Calculator/Calc3Unities/index.jsx
+++ src/components/Calculator/Calc3Unities/index.jsx
@@ -1,45 +1,12 @@
+import { Container, Nota } from "./styles";
 import { useState } from "react";
-
+import { calcularMedia3 } from "../../../features/CalcularNotas3";
 export function Calc3Unities() {
     const [dado1, setDado1] = useState(1.0);
     const [dado2, setDado2] = useState(1.0);
     const [dado3, setDado3] = useState(1.0);
 
-    function calcularMedia3(N1, N2, N3) {
-        var media = 5;
-        N1 = parseFloat(N1)
-        N2 = parseFloat(N2)
-        N3 = parseFloat(N3)
-        var mediaDoAluno = (N1 + N2 + N3) / 3
-        if (N1 == null) {
-            alert("Digite um valor válido.");
-        } else {
-            if (N1 < 3 || N2 < 3) {
-                media = 7;
-            } else if (N3 < 3) {
-                media = 7;
-            }
-            console.log(mediaDoAluno, media)
-            if (mediaDoAluno >= media) {
-                alert("Você passou!");
-            } else {
-                let maiorValor = Math.max(N1, N2, N3)
-                let segundoMaior;
-                if (maiorValor===N1) {
-                    segundoMaior = Math.max(N2,N3)
-                } else if(maiorValor===N2){
-                    segundoMaior = Math.max(N1,N3)
-                } else if(maiorValor===N3) {
-                    segundoMaior = Math.max(N1,N2)
-                }
-                console.log(media * 3, maiorValor, segundoMaior)
-                var recNota = (media * 3) - (maiorValor + segundoMaior)
-                alert(`Você tem problemas! \nNota necessária na recuperação: ${recNota}`);
-            }
-        }
-    }
-
-
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -48,24 +15,38 @@ export function Calc3Unities() {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    value={dado1}
-                    onChange={(e) => setDado1(e.target.value)} // 2. e 3.
-                    placeholder="Dado 1"
-                />
-                <input
-                    value={dado2}
-                    onChange={(e) => setDado2(e.target.value)} // 2. e 3.
-                    placeholder="Dado 2"
-                />
-                <input
-                    value={dado3}
-                    onChange={(e) => setDado3(e.target.value)} // 2. e 3.
-                    placeholder="Dado 3"
-                />
-                <button type="submit">Enviar</button>
-            </form>
+            <Container>
+
+
+                <form onSubmit={handleSubmit}>
+                    <Nota>
+                        <p>N1: </p>
+                    <input
+                        value={dado1}
+                        onChange={(e) => setDado1(e.target.value)} // 2. e 3.
+                        placeholder="Dado 1"
+                    />
+                    </Nota>
+                    <Nota>
+                        <p>N2: </p>
+                    <input
+                        value={dado2}
+                        onChange={(e) => setDado2(e.target.value)} // 2. e 3.
+                        placeholder="Dado 2"
+                    />    
+                    </Nota>
+                    <Nota>
+                        <p>N3: </p>
+                    <input
+                        value={dado3}
+                        onChange={(e) => setDado3(e.target.value)} // 2. e 3.
+                        placeholder="Dado 3"
+                    />    
+                    </Nota>
+                    
+                    <button type="submit">Enviar</button>
+                </form>
+            </Container>
         </>)
 }
 
