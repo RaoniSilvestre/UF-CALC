87e76f4ce7ca2cb2950724111188c8d77141fa82
Raoni Silva <raoni.silvestre@hotmail.com>
2023-09-06 10:55:47 -0300

    estilo para componente calculadora 2 unidades

---
+++ src/components/Calculator/Calc2Unities/styled.js
@@ -0,0 +1,23 @@
+import { styled } from "styled-components"
+
+export const Container = styled.div`
+    margin-top: 1rem;
+    button {
+        color: white;
+        border: 0;
+        border-radius: 0.25rem;
+        background-color: #164194;
+        padding: 0.5rem;
+    }
+`
+
+export const Nota = styled.div`
+    padding:0.4rem;
+    display: flex;
+    justify-items: center;
+    justify-content: space-evenly;
+    input {
+        padding: 0.5rem;
+    }
+    
+`
